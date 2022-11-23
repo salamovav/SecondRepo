3db023a681d801996204c5c483d6a996fb2c2974
Tora <109001487+salamovav@users.noreply.github.com>
2022-11-24 00:25:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// console.log(03);
+// console.log("03");
+// console.log('Добро '+'пожаловать '+'на курс '+'Viktoria');
+// alert('14.11.2022');
+
+document.getElementById('one').innerHTML='Hello world!'
+
+
+typeof undefined;//underfined
+console.log(typeof underfined);
+typeof 0;//number
+console.log(typeof 0);
+typeof 10n;//bigint
+console.log(typeof 10n);
+typeof true;//boolean
+console.log(typeof true);
+typeof 'Мама';//string
+console.log(typeof 'Мама');
+typeof Symbol('id');//symbol
+console.log(typeof Symbol('id'));
+typeof null;object
+console.log(typeof null);
